2e09bc59d151d80fb5309abe97e0250cec8c4815
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-10-31 12:40:31 -0400

    Halloween Test second change

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,5 +2,8 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("I like halloween");
         System.out.println("I am about to cry");
+        System.out.println("What the hack!!!");
+        System.out.println("No woman no cry");
+        System.out.println("Why do you have existential questions ");
     }
 }
