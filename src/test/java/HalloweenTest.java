82451bb3936a0b244326306d8f8e9434784260ef
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-10-31 11:58:13 -0400

    Halloween Test first change

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,6 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("I like halloween");
+        System.out.println("I am about to cry");
     }
 }
